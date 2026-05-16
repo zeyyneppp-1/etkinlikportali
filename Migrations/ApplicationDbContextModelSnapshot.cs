38174d473c730c2d599e839c46f5739d49b7cfd0
zeyyneppp-1 <zeynep.ciftci2@ogr.sakarya.edu.tr>
2026-05-16 11:38:11 +0300

    Migrations: Entity Framework Core Code-First tablolarını veritabanına aktaran göç (migration) dosyaları oluşturuldu.

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
+
 #nullable disable
 
 namespace EtkinlikPortali.Migrations
